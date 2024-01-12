f4eee7e0a8e9d0496d17a2632780aa8fea89a7ea
Sohail <sohailkarmani16@gmail.com>
2024-01-12 14:41:00 +0500

    added dataInsert method

---
+++ DatabaseConnection.java
@@ -0,0 +1,35 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/contactdata", "root", "");
+                System.out.println("------------------Connected To The DataBase------------------");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Contact_Manager app = new Contact_Manager();
+        app.start();
+    }
+}
